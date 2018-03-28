10ec25a455b9404be439fc42c7ef6aedb273cae5
РасторгуевА <@227_5>
2018-03-28 17:06:11 +0600

    Add project files.

---
+++ Project6/Source1.cpp
@@ -0,0 +1,51 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+#include <locale.h>
+#include <iostream>
+#include "Header.h"
+#include<Windows.h>
+
+void main(/*int args, char *argv[]*/)
+{
+	system("chcp 1251");
+	system("cls");
+
+	srand(time(0));
+
+
+	/*if (argv > 0)
+	{
+	for (int i = 0; i < args; i++)
+	{
+	printf("[%d] - %s\n", i, argv[i]);
+	}
+	}*/
+
+	printf("Введите кол-во элементов: ");
+
+	int count = 0;
+	scanf_s("%d", &count);
+
+	crypt *Crypt = (crypt*)malloc(sizeof(crypt) * count);
+	if (Crypt != NULL)
+	{
+		for (int i = 0; i < count; i++)
+			generationCrypt((Crypt + i));
+
+		for (int i = 0; i < count; i++)
+		{
+			printf("подразделение: %d%d%d\n", Crypt->razd[0], Crypt->razd[1], Crypt->razd[2]);
+			for (int j = 0; j < 4; j++)
+			{
+				printf("-->%2-%2lf-%d\n",
+					Crypt->fact[j][1],
+					Crypt->plan[j][1],
+					Crypt->plan[j][2]);
+	           
+			}
+		}
+	}
+	system("pause");
+
+}
